045df5a5b12fea8a208a8509cad3460a28d7c4d6
allthatnet <githoob@allthatnet.com>
2018-03-10 21:15:38 -0500

    Remove Ansible Galaxy roles and add them to the provisioning script

--- provision_production.sh
+++ provision_production.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+
 cd provisioning
-ansible-playbook -i hosts --vault-password-file=~/.cuttlefish_ansible_vault_pass.txt -u root playbook.yml
+
+galaxy_roles=(
+	"rvm_io.ruby,v2.0.1"
+	"ANXS.postgresql,v1.9.1"
+	"geerlingguy.certbot,3.0.0"
+	"DavidWittman.redis,1.2.5"
+)
+
+echo -e "Setting Ansible Galaxy roles...\n"
+for r in "${galaxy_roles[@]}"
+do
+	ansible-galaxy install --roles-path=./roles $r
+done 
+
+if [[ $? -eq 0 ]]
+then
+	echo -e "Provisioning the server...\n"
+	ansible-playbook -i hosts --vault-password-file=~/.cuttlefish_ansible_vault_pass.txt -u root playbook.yml
+fi
